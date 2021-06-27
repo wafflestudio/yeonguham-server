36e34532135422488ebe047cdc5823e8412a385c
silvermanseoul <silverman0822@gmail.com>
2021-06-27 23:32:17 +0900

    chore: settings.py, wsgi.py, asgi.py 분리

---
+++ yeonguham/settings/dev.py
@@ -0,0 +1,19 @@
+import os
+import json
+import sys
+
+from base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS += []
+
+WSGI_APPLICATION = "yeonguham.wsgi.dev.application"
+
+SECRETS_FILE = os.path.join(BASE_DIR, "secrets-dev.json")
+
+secrets = json.loads(open(SECRETS_FILE).read())
+for key, value in secrets.items():
+    setattr(sys.modules[__name__], key, value)
