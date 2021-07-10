ecdec97d0662ee35f544eb9527c5178b0920073f
younjoo <younjoo0614@snu.ac.kr>
2021-07-10 16:35:39 +0900

    fix: Researchee, Researcher 통합해서 Profile 모델로 변경

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-from datetime import datetime, date
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
 
-
-class Researchee(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=20, blank=True)
-    birth = models.DateField(auto_now=False, auto_now_add=False)
-    profileImage = models.ImageField(max_length=255, blank=True, null=True)
+    profileImage = models.ImageField(max_length=None, blank = True, null = True)
+    lab = models.CharField(max_length=50, blank=True)
     TAG_CHOICES = [
         ("MEDICAL", "Medical"),
         ("BIO_SCI", "Bio_Sci"),
@@ -37,30 +34,10 @@ class Researchee(models.Model):
     )
 
     @receiver(post_save, sender=User)
-    def create_user_researchee(self, sender, instance, created, **kwargs):
+    def create_user_researchee(sender, instance, created, **kwargs):
         if created:
-            Researchee.objects.create(user=instance)
+            Profile.objects.create(user=instance)
 
     @receiver(post_save, sender=User)
-    def save_user_researchee(self, sender, instance, **kwargs):
+    def save_user_researchee(sender, instance, **kwargs):
         instance.researchee.save()
-
-    def get_age(self):
-        today = date.today()
-        return int((today - self.birth).days / 365)
-
-
-class Researcher(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=20, blank=True)
-    lab = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(max_length=100)
-
-    @receiver(post_save, sender=User)
-    def create_user_researcher(self, sender, instance, created, **kwargs):
-        if created:
-            Researcher.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_Researcher(self, sender, instance, **kwargs):
-        instance.researcher.save()
